34e61949c8a40ae2957cc137593776e46be466fe
sanga327 <dltkddk327@naver.com>
2019-12-11 19:09:33 +0900

    최초

--- Chatbot/01_auto_browser.py
+++ Chatbot/01_auto_browser.py
@@ -1,7 +1,5 @@
 import webbrowser
 
-artists=['민수','백예린','방탄소년단']
+artists=['민수','백예린','방탄소년단','이상아']
 for artist in artists:
     webbrowser.open('https://search.naver.com/search.naver?query='+artist)
-
-
